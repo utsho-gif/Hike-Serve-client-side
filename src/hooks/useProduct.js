2c28d32ce012f94db51f4a2d3785eba5e9994e6f
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-05-11 05:36:11 +0600

    update quantity without reload

--- src/hooks/useProduct.js
+++ src/hooks/useProduct.js
@@ -2,14 +2,15 @@ import { useEffect, useState } from "react"
 
 const useProduct = () => {
     const [products, setProducts] = useState([]);
+    const [isReload, setReload] = useState(false);
 
     useEffect(() => {
-        fetch('https://aqueous-taiga-75883.herokuapp.com/product')
+        fetch('http://localhost:5000/product')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
 
-    return [products, setProducts];
+    return [products, setProducts, isReload, setReload];
 }
 
 export default useProduct;
--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,60 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import useProduct from "../../hooks/useProduct";
 import "./Inventory.css";
 
 const Inventory = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
-  const [item, setItem] = useState({});
+  // const [products, isReload, setReload] = useProduct();
+  const [isReload, setReload] = useState(false);
   const { _id, name, img, description, price, quantity, supplier, sold } =
     product;
 
-    const url = `https://aqueous-taiga-75883.herokuapp.com/inventory/${id}`;
-    useEffect(() => {
-        fetch(url)
-      .then((res) => res.json())
-      .then((data) => setProduct(data));
-  }, [url]);
   useEffect(() => {
+    const url = `http://localhost:5000/inventory/${id}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setItem(data));
-  }, [url, setItem]);
-  const handleUpdateQ = () => {
-    const { quantity, ...rest } = item;
-    const newQuantity = JSON.stringify(quantity - 1);
-    const newValue = { quantity: `${newQuantity}`, ...rest };
-    setItem(newValue);
-    //send to server
-    const url = `https://aqueous-taiga-75883.herokuapp.com/inventory/${id}`;
+      .then((data) => setProduct(data));
+  }, [isReload]);
+
+  const handleUpdateQ = (quantity) => {
+    const delivery = parseInt(quantity);
+    const newDelivery = delivery - 1;
+
+    // console.log(newDelivery);
+
+    //send to the server
+    const url = `http://localhost:5000/inventory/${id}`;
     fetch(url, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "content-type": "application/json",
+        'content-type' : 'application/json',
       },
-      body: JSON.stringify({ newValue }),
+      body: JSON.stringify({quantity: newDelivery})
     })
-      .then((res) => res.json())
-      .then((data) => setItem(data));
-    window.location.reload();
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      // setProduct(data);
+      setReload(!isReload); 
+      // {isReload}
+    })
+
+
   };
-  const handleRestoke = event => {
-      const upQuantity = parseInt(event.target.restoke.value);
-      const {quantity, ...rest} = item;
-      const newQuantity = parseInt(quantity) + upQuantity;
-      const newValue = {quantity: `${newQuantity}`, ...rest};
-      setItem(newValue);
-      const url = `https://aqueous-taiga-75883.herokuapp.com/inventory/${id}`;
-      fetch(url, {
-          method: 'PUT',
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({newValue})
-      })
-      .then(res => res.json())
-      .then(data => setItem(data))
-  }
+  const handleRestoke = (event) => {};
   return (
     <div className="container">
       <h3 className="mt-5">Update the product</h3>
@@ -68,34 +57,44 @@ const Inventory = () => {
           className="w-50"
         ></div>
       </div>
-      <div className="my-5 p-2" style={{ border: "2px solid #36D7B7", borderRadius: "10px" }}>
       <div
-        className="d-flex justify-content-center px-0 my-5"
+        className="my-5 p-2"
+        style={{ border: "2px solid #36D7B7", borderRadius: "10px" }}
       >
-        <div>
-          <img src={img} alt="" />
-        </div>
-        <div className="ms-5">
-          <div className="text-start">
-            <h4>{name}</h4>
-            <h6>Product Id: {_id}</h6>
-            <h6>Quantity: {quantity}</h6>
-            <h6>Supplier: </h6>
+        <div className="d-flex justify-content-center px-0 my-5">
+          <div>
+            <img src={img} alt="" />
+          </div>
+          <div className="ms-5">
+            <div className="text-start">
+              <h4>{name}</h4>
+              <h6>Product Id: {_id}</h6>
+              <h6>Quantity: {quantity}</h6>
+              <h6>Supplier: </h6>
+            </div>
+          </div>
+          <div>
+            <button
+              className="btn btn-outline-danger btn-mod ms-3 mt-4"
+              onClick={() => handleUpdateQ(quantity)}
+            >
+              Delivery
+            </button>
           </div>
         </div>
-        <div>
-          <button
-            className="btn btn-outline-danger btn-mod ms-3 mt-4"
-            onClick={handleUpdateQ}
-          >
-            Delivery
-          </button>
-        </div>
-      </div>
-          <form onSubmit={handleRestoke}>
-            <input className="d-block mx-auto re-mod ps-5" type="text" name="restoke" placeholder="Restoke Quantity"/>
-            <input className="mt-2 btn btn-outline-success" type="submit" value="Restoke" />
-          </form>
+        <form onSubmit={handleRestoke}>
+          <input
+            className="d-block mx-auto re-mod ps-5"
+            type="text"
+            name="restoke"
+            placeholder="Restoke Quantity"
+          />
+          <input
+            className="mt-2 btn btn-outline-success"
+            type="submit"
+            value="Restoke"
+          />
+        </form>
       </div>
     </div>
   );
