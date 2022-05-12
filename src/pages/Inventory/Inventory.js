fe1a06ef860fc3d033702374caf1fcfb33b7d762
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-05-12 16:18:21 +0600

    added restock

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import useProduct from "../../hooks/useProduct";
 import "./Inventory.css";
 
 const Inventory = () => {
   const { id } = useParams();
+  const stockRef = useRef("");
   const [product, setProduct] = useState({});
-  // const [products, isReload, setReload] = useProduct();
   const [isReload, setReload] = useState(false);
+  const [error, setError] = useState("");
   const { _id, name, img, description, price, quantity, supplier, sold } =
     product;
 
@@ -18,32 +19,57 @@ const Inventory = () => {
       .then((data) => setProduct(data));
   }, [isReload]);
 
+  const url = `http://localhost:5000/inventory/${id}`;
   const handleUpdateQ = (quantity) => {
     const delivery = parseInt(quantity);
     const newDelivery = delivery - 1;
 
     // console.log(newDelivery);
 
-    //send to the server
-    const url = `http://localhost:5000/inventory/${id}`;
+    //send to server
     fetch(url, {
-      method: 'PUT',
+      method: "PUT",
       headers: {
-        'content-type' : 'application/json',
+        "content-type": "application/json",
       },
-      body: JSON.stringify({quantity: newDelivery})
+      body: JSON.stringify({ quantity: newDelivery }),
     })
-    .then(res => res.json())
-    .then(data => {
-      // console.log(data);
-      // setProduct(data);
-      setReload(!isReload); 
-      // {isReload}
-    })
-
-
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setReload(!isReload);
+      });
   };
-  const handleRestoke = (event) => {};
+
+  const handleSub = (event) => {
+    event.preventDefault();
+    event.target.reset();
+  };
+
+  const handleRestock = (quantity) => {
+    const stock = parseInt(stockRef.current.value);
+    // console.log(typeof stock);
+    if (typeof stock === "number") {
+      const newStock = stock;
+      const restock = quantity + newStock;
+
+      //send to server
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ quantity: restock }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setReload(!isReload);
+        });
+    } else {
+      setError("Please input a valid number");
+    }
+  };
   return (
     <div className="container">
       <h3 className="mt-5">Update the product</h3>
@@ -58,42 +84,48 @@ const Inventory = () => {
         ></div>
       </div>
       <div
-        className="my-5 p-2"
+        className="my-5 p-4 width-mod mx-auto"
         style={{ border: "2px solid #36D7B7", borderRadius: "10px" }}
       >
-        <div className="d-flex justify-content-center px-0 my-5">
-          <div>
-            <img src={img} alt="" />
-          </div>
+        <div className="d-flex justify-content-center px-0 my-5 mx-auto">
           <div className="ms-5">
             <div className="text-start">
-              <h4>{name}</h4>
-              <h6>Product Id: {_id}</h6>
-              <h6>Quantity: {quantity}</h6>
-              <h6>Supplier: </h6>
+              <img src={img} alt="" />
+              <div className="my-4">
+                <h4>{name}</h4>
+                <h6>Product Id: {_id}</h6>
+                <h6>Quantity: {quantity}</h6>
+                <h6>Supplier: </h6>
+              </div>
+              <div>
+                <button
+                  className="btn btn-outline-danger btn-mod"
+                  onClick={() => handleUpdateQ(quantity)}
+                >
+                  Delivery
+                </button>
+              </div>
             </div>
           </div>
-          <div>
-            <button
-              className="btn btn-outline-danger btn-mod ms-3 mt-4"
-              onClick={() => handleUpdateQ(quantity)}
-            >
-              Delivery
-            </button>
-          </div>
+          <div></div>
         </div>
-        <form onSubmit={handleRestoke}>
+        <form onSubmit={handleSub}>
           <input
-            className="d-block mx-auto re-mod ps-5"
+            className="d-block mx-auto re-mod ps-3"
             type="text"
-            name="restoke"
-            placeholder="Restoke Quantity"
+            name="restock"
+            placeholder="&#x21E2; Restock Quantity"
+            ref={stockRef}
+            pattern="[0-9]*"
+            autoComplete="off"
           />
-          <input
-            className="mt-2 btn btn-outline-success"
-            type="submit"
-            value="Restoke"
-          />
+          {error} <br />
+          <button
+            className="btn btn-outline-success mt-3 border-3"
+            onClick={() => handleRestock(quantity)}
+          >
+            Restock
+          </button>
         </form>
       </div>
     </div>
